0fa1e42aa00d9b580260ae7a719e7a9f6f17f8d3
XCyberCode <mat.vasenin@yandex.ru>
2024-02-04 10:21:30 +0300

    Update sysconf.h

--- main/core/sysconf.h
+++ main/core/sysconf.h
@@ -1,10 +1,13 @@
 // Pins parameters
-#define CON1_GPIO GPIO_NUM_26
-#define CON2_GPIO GPIO_NUM_27
-#define CON3_GPIO GPIO_NUM_25
+#define CON1_GPIO GPIO_NUM_27 // Left button
+#define CON2_GPIO GPIO_NUM_25 // Center button
+#define CON3_GPIO GPIO_NUM_26 // Right button
 
 // Matrix parameters
 #define MCON1_GPIO GPIO_NUM_32
 #define MATRIX_WIDTH 8
 #define MATRIX_LENGTH 8
 #define MATRIX_AREA MATRIX_LENGTH * MATRIX_WIDTH
+
+// System configuration
+#define DEFAULT_BRIGHTNESS 1 // From 1 ... 25
