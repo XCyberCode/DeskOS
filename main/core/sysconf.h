10ee238b0c07f7d19930d2f1c0876d868b41611e
XCyberCode <mat.vasenin@yandex.ru>
2024-02-04 19:18:53 +0300

    Update system conf

--- main/core/sysconf.h
+++ main/core/sysconf.h
@@ -1,7 +1,11 @@
-// Pins parameters
-#define CON1_GPIO GPIO_NUM_26 // Left button
-#define CON2_GPIO GPIO_NUM_25 // Center button
-#define CON3_GPIO GPIO_NUM_27 // Right button
+// GPIO configuration
+
+// Left button
+#define CON1_GPIO GPIO_NUM_26
+// Center button
+#define CON2_GPIO GPIO_NUM_25
+// Right button
+#define CON3_GPIO GPIO_NUM_27
 
 // Matrix parameters
 #define MCON1_GPIO GPIO_NUM_32
