cb1e1e8721ccf9e443bb0c8495343a84565162ea
XCyberCode <mat.vasenin@yandex.ru>
2024-02-10 21:48:05 +0300

    Optimize init process

--- main/core/init.c
+++ main/core/init.c
@@ -9,29 +9,10 @@ void setup_device(led_strip_handle_t matrix_handle, nvs_handle_t nvs_storage_han
 {
     ESP_ERROR_CHECK(nvs_set_u8(nvs_storage_handle, "init_state", 0));
 
-    uint8_t matrix_brightness = 10;
+    uint8_t matrix_brightness = DEFAULT_BRIGHTNESS;
     while(1)
     {
-        led_matrix_draw_h_line
-        (
-            matrix_handle, 0, 0, 8, 
-            matrix_brightness * 10, matrix_brightness * 10, matrix_brightness * 10
-        );
-        led_matrix_draw_h_line
-        (
-            matrix_handle, 0, 7, 8, 
-            matrix_brightness * 10, matrix_brightness * 10, matrix_brightness * 10
-        );
-        led_matrix_draw_v_line
-        (
-            matrix_handle, 0, 0, 8, 
-            matrix_brightness * 10, matrix_brightness * 10, matrix_brightness * 10
-        );
-        led_matrix_draw_v_line
-        (
-            matrix_handle, 7, 0, 8, 
-            matrix_brightness * 10, matrix_brightness * 10, matrix_brightness * 10
-        );
+        led_matrix_draw_rect(matrix_handle, 0, 0, 8, 8, matrix_brightness * 10, matrix_brightness * 10, matrix_brightness * 10);
         ESP_ERROR_CHECK(led_strip_refresh(matrix_handle));
 
         if(gpio_get_level(CON1_GPIO) && matrix_brightness > 0)
