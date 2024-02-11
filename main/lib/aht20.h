be08b27f40da552c72396e697fd7dd7898a2c145
XCyberCode <mat.vasenin@yandex.ru>
2024-02-11 11:24:12 +0300

    Update aht20 driver

--- main/lib/aht20.h
+++ main/lib/aht20.h
@@ -5,5 +5,7 @@
 // Include core components
 #include <driver/i2c.h>
 
-void aht20_init(i2c_config_t i2c_config);
+void aht20_init(i2c_config_t aht20_config);
+void aht20_get_temperature(i2c_config_t aht20_config, int16_t *temperature);
+void aht20_get_humidity(i2c_config_t aht20_config, uint8_t *humidity);
 #endif
