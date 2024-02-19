7ca5cbb5c0dc35685fcdaec43f28fd032a19b40d
XCyberCode <mat.vasenin@yandex.ru>
2024-02-19 17:34:11 +0300

    Update sensors component

--- main/core/sensors.h
+++ main/core/sensors.h
@@ -5,9 +5,13 @@
 // Include internal libraries
 #include <inttypes.h>
 
-void update_sensors();
-int16_t sensor_get_temp();
-uint8_t sensor_get_humidity();
-uint16_t sensor_get_pressure();
-uint16_t sensor_get_altitude();
+// Include core components
+#include <aht20.h>
+#include <bmp280.h>
+
+void update_sensors(void * task_parameters);
+void sensor_get_temp(int16_t * temperature);
+void sensor_get_humidity(uint8_t * humidity);
+void sensor_get_pressure(uint16_t * pressure);
+void sensor_get_altitude(uint16_t * altitude);
 #endif
