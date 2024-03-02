c88f283dc45e31d46d84b41f495100b1cb71b764
XCyberCode <mat.vasenin@yandex.ru>
2024-03-02 20:21:10 +0300

    Fix naming

--- main/module/bmp280.c
+++ main/module/bmp280.c
@@ -5,8 +5,8 @@ void bmp280_init(bmx_config_t sensor_config) {}
 
 void bmp280_get_temperature(bmx_config_t sensor_config, int16_t *temperature) {}
 
-void bmp280_get_pressure(bmx_config_t sensor_config, uint8_t *humidity) {}
+void bmp280_get_pressure(bmx_config_t sensor_config, uint16_t *pressure) {}
 
 void bmp280_get_altitude(bmx_config_t sensor_config, uint16_t *altitude) {}
 
-void bme280_get_altitude(bmx_config_t sensor_config, uint16_t *humidity) {}
+void bme280_get_humidity(bmx_config_t sensor_config, uint8_t *humidity) {}
