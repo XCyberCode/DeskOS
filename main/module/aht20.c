af62e233b7d3c296a96bbfe1cdf873c4c55b742d
XCyberCode <mat.vasenin@yandex.ru>
2024-03-04 18:33:47 +0300

    Update aht20 driver

--- main/module/aht20.c
+++ main/module/aht20.c
@@ -1,7 +1,10 @@
 // Include header file
 #include <aht20.h>
 
-void aht20_init(i2c_config_t aht20_config){}
+void aht20_init(i2c_config_t aht20_config)
+{
+    
+}
 
 void aht20_get_temperature(i2c_config_t aht20_config, int16_t *temperature){}
 
--- main/module/aht20.h
+++ main/module/aht20.h
@@ -4,18 +4,26 @@
 
 // Include core components
 #include <driver/i2c.h>
+#include <esp_err.h>
+#include <esp_log.h>
+
+// Sensor configuration structure
+typedef struct
+{
+    i2c_config_t bus_config; // I2C bus config
+} ahtx_config_t;
 
 // Initialize AHT20 sensor
-// @param aht20_config structure with sensor configuration
+// @param aht20_config sensor configuration structure
 void aht20_init(i2c_config_t aht20_config);
 
 // Get temperature
-// @param aht20_config structure with sensor configuration
+// @param aht20_config sensor configuration structure
 // @param *temperature pointer to temperature variable
 void aht20_get_temperature(i2c_config_t aht20_config, int16_t *temperature);
 
 // Get humidity
-// @param aht20_config structure with sensor configuration
+// @param aht20_config sensor configuration structure
 // @param *humidity pointer to humidity variable
 void aht20_get_humidity(i2c_config_t aht20_config, uint8_t *humidity);
 #endif
