7c9c403c1d95030d0a1947206ee6f2cc21ce21cd
XCyberCode <mat.vasenin@yandex.ru>
2024-03-23 17:07:20 +0300

    Update code structure

--- main/core/sensors.h
+++ main/core/sensors.h
@@ -2,15 +2,13 @@
 #ifndef CORE_SENSORS_HEADER_FLAG
 #define CORE_SENSORS_HEADER_FLAG
 
-// Include internal libraries
-#include <inttypes.h>
-
 // Include core components
 #include <aht20.h>
 #include <bmp280.h>
 #include <freertos/FreeRTOS.h>
+#include <sysconf.h>
 
-void sensor_update_task(void *pvParameters);
+void sensor_update_task(void *task_parameters);
 void sensor_get_temp(int16_t *temperature);
 void sensor_get_humidity(uint8_t *humidity);
 void sensor_get_pressure(uint16_t *pressure);
