ac1954b3592c3c13ababf58d235ecd60b48347d0
XCyberCode <mat.vasenin@yandex.ru>
2024-04-16 14:42:11 +0300

    Обновлены комментарии

--- main/module/bmp280.c
+++ main/module/bmp280.c
@@ -3,8 +3,8 @@
 
 void bmp_compensate_pressure(bmp_config_t sensor_config, int32_t raw_pressure, int32_t raw_temp, double *pressure)
 {
-    // Comprensate pressure data using calibration values
-    // This formula is available in the official datasheet
+    // Осуществление компенсации измерений атмосферного давления
+    // посредством данных, прочитанных из калибровочных регистров
     double calib_var1, calib_var2, current_pressure;
     calib_var1 = ((double)raw_temp / 2.0) - 64000.0;
     calib_var2 = calib_var1 * calib_var1 * ((double)sensor_config.calibration_data.P6) / 32768.0;
@@ -14,7 +14,7 @@ void bmp_compensate_pressure(bmp_config_t sensor_config, int32_t raw_pressure, i
         ((double)sensor_config.calibration_data.P2) * calib_var1) / 524288.0;
     calib_var1 = (1.0 + calib_var1 / 32768.0) * ((double)sensor_config.calibration_data.P1);
     
-    // Prevent dividing by zero
+    // Предотвращение деления на ноль
     if (calib_var1 == 0.0) 
     {
         *pressure = 0;
@@ -32,8 +32,8 @@ void bmp_compensate_pressure(bmp_config_t sensor_config, int32_t raw_pressure, i
 
 void bmp_compensate_temperature(bmp_config_t sensor_config, int32_t raw_temp, int32_t *temperature)
 {
-    // Comprensate temperature data using calibration values
-    // This formula is available in the official datasheet
+    // Осуществление компенсации измерений температуры
+    // посредством данных, прочитанных из калибровочных регистров
     double calib_var1 = (((double)raw_temp) / 16384.0 - ((double)sensor_config.calibration_data.T1) / 1024.0) * ((double)sensor_config.calibration_data.T2);
     double calib_var2 = ((((double)raw_temp) / 131072.0 - ((double)sensor_config.calibration_data.T1) / 8192.0) *
     (((double)raw_temp) / 131072.0 - ((double)sensor_config.calibration_data.T1) / 8192.0)) * ((double)sensor_config.calibration_data.T3);
@@ -42,11 +42,11 @@ void bmp_compensate_temperature(bmp_config_t sensor_config, int32_t raw_temp, in
 
 esp_err_t bmp_init(bmp_config_t *sensor_config) 
 {
-    // Prepare for transmission
+    // Подготовка к передачи данных
     uint8_t transmit_data[2] = {BMP280_REG_CHIP_ID};
     uint8_t received_data[1] = {0};
 
-    // Get chip_id of the sensor
+    // Получение ID чипа из памяти датчика
     // 0x60 - BME280, 0x58 - BMP280
     ESP_ERROR_CHECK(i2c_master_transmit(sensor_config->dev_handle, transmit_data, 1, -1));
     ESP_ERROR_CHECK(i2c_master_receive(sensor_config->dev_handle, received_data, 1, -1));
@@ -56,36 +56,35 @@ esp_err_t bmp_init(bmp_config_t *sensor_config)
         return ESP_ERR_NOT_FOUND;
     }
 
-    // Sensor software reset
+    // Осуществление программной перезагрузки датчика
     transmit_data[0] = BMP280_REG_RESET;
     transmit_data[1] = BMP280_RESET_VALUE;
     ESP_ERROR_CHECK(i2c_master_transmit(sensor_config->dev_handle, transmit_data, 1, -1));
     ESP_ERROR_CHECK(i2c_master_transmit(sensor_config->dev_handle, transmit_data, 1, -1));
     
-    // Wait sensor
-    transmit_data[0] = BMP280_REG_STATUS;
+    // Ожидание датчика
     vTaskDelay(100 / portTICK_PERIOD_MS);
 
-    // Prepare sensor
+    // Получение калибровочных данных с датчика
     bmp_read_calibration_data(sensor_config);
 
-    // Configure filtering and the delay between measurements
+    // Настройка фильтра и задержки между измерениями
     transmit_data[0] = BMP280_REG_CONFIG;
     transmit_data[1] = (sensor_config->standby_time << 5) | (sensor_config->filter << 2);
     ESP_ERROR_CHECK(i2c_master_transmit(sensor_config->dev_handle, transmit_data, 2, -1));
     
-    // Configure oversampling and working mode
+    // Настройка оверсемплинга (oversample) и режима работы
     transmit_data[0] = BMP280_REG_CONTROL;
     transmit_data[1] = (sensor_config->osrs_temp << 5) | (sensor_config->osrs_press << 2) | (sensor_config->mode);
     ESP_ERROR_CHECK(i2c_master_transmit(sensor_config->dev_handle, transmit_data, 2, -1));
 
-    // Return state
+    // Вернуть состояние
     return ESP_OK;
 }
 
 esp_err_t bmp_read_calibration_data(bmp_config_t *sensor_config)
 {
-    // Read calibration data. It is required for compensation of the measurements
+    // Чтение калибровочных данных. Эти данные необходимы для компенсации измерений
     uint8_t transmit_data[1] = {BMP280_REG_CALIBRATE};
     uint8_t received_data[24];
     ESP_ERROR_CHECK(i2c_master_transmit(sensor_config->dev_handle, transmit_data, 1, -1));
@@ -107,18 +106,18 @@ esp_err_t bmp_read_calibration_data(bmp_config_t *sensor_config)
 
 esp_err_t bmp_read_raw_temperature(bmp_config_t sensor_config, int32_t *temperature) 
 {
-    // Read data from the sensor
+    // Чтение "сырых" данных о температуре с датчика
     uint8_t transmit_data[1] = {BMP280_REG_TEMP_MSB};
     uint8_t received_data[3];
     ESP_ERROR_CHECK(i2c_master_transmit(sensor_config.dev_handle, transmit_data, 1, -1));
     ESP_ERROR_CHECK(i2c_master_receive(sensor_config.dev_handle, received_data, 3, -1));
     int32_t raw_temp = ((uint32_t)received_data[0] << 16) | ((uint32_t)received_data[1] << 8) | (uint32_t)received_data[2];
+    raw_temp >>= 4; // Сдвиг. Необходим для MSB, LSB и XLSB
 
-    // Process data
-    raw_temp >>= 4;
+    // Осуществить компенсацию температуры
     bmp_compensate_temperature(sensor_config, raw_temp, temperature);
 
-    // Return state
+    // Вернуть состояние
     return ESP_OK;
 }
 
@@ -134,27 +133,28 @@ esp_err_t bmp_read_temperature(bmp_config_t sensor_config, double *temperature)
 
 esp_err_t bmp_read_pressure(bmp_config_t sensor_config, double *pressure) 
 {
-    // Read processed temperature. It is required for pressure compensation
+    // Чтение необработанных данных о температуре с датчика
     int32_t raw_temp;
     ESP_ERROR_CHECK(bmp_read_raw_temperature(sensor_config, &raw_temp));
 
-    // Read raw pressure data from the sensor
+    // Чтение "сырых" дынных о давлении с датчика
     uint8_t transmit_data[1] = {BMP280_REG_PRESS_MSB};
     uint8_t received_data[3];
     ESP_ERROR_CHECK(i2c_master_transmit(sensor_config.dev_handle, transmit_data, 1, -1));
     ESP_ERROR_CHECK(i2c_master_receive(sensor_config.dev_handle, received_data, 3, -1));
     int32_t raw_press = (received_data[0] << 16) | (received_data[1] << 8) | received_data[2];
-    raw_press >>= 4;
+    raw_press >>= 4; // Сдвиг. Необходим для MSB, LSB и XLSB
 
-    // Compensate pressure using the formula
+    // Осуществить компенсацию давления
     bmp_compensate_pressure(sensor_config, raw_press, raw_temp, pressure);
 
-    // Return state
+    // Вернуть состояние
     return ESP_OK;
 }
 
 esp_err_t bmp_get_altitude(bmp_config_t sensor_config, uint16_t *altitude) 
 {
+    // Вернуть результат выполнения
     return ESP_OK;
 }
 
